1cd90fd055a5f65748f66e4f8272db27261156f2
unknown <lewislewis996@gmail.com>
2022-05-26 01:02:54 +0200

    feat exemple for use getmessage hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useGetMessageQuery } from './services';
+interface responses {
+  message: string;
+  id: number;
+}
 
 function App() {
+  const { data, isLoading, isError, isSuccess } = useGetMessageQuery();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error!</div>;
+  }
+  const messageList = data?.map((item: responses) => {
+    return <li key={item.id}>{item.message}
+    </li>
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {messageList}
+      </ul>
     </div>
   );
 }
